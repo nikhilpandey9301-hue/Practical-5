5aa3f62b5e6d3310a58881be9f17bfeaeea14cb2
Nikhilapandey31 <nikhilpandey.31.8859@gmail.com>
2026-09-04 23:23:39 +0530

    ready to deploy

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// GET
+app.get("/", (req, res) => {
+    res.send("Node.js Server is Running");
+});
+
+// GET
+app.get("/students", (req, res) => {
+    res.send("Get all students");
+});
+
+// POST
+app.post("/students", (req, res) => {
+    res.send("Student added");
+});
+
+// PUT
+app.put("/students", (req, res) => {
+    res.send("Student completely updated");
+});
+
+// PATCH
+app.patch("/updatestudents", (req, res) => {
+    res.send("Student partially updated");
+});
+
+// DELETE
+app.delete("/deletestudents", (req, res) => {
+    res.send("Student deleted");
+});
+
+app.listen(8800, () => {
+    console.log("Server running on port 8800");
+});
